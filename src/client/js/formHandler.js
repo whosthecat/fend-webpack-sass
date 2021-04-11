3b9b256e97dc99679030b53e1730cb6e87dda868
whosthecat <79358806+whosthecat@users.noreply.github.com>
2021-04-11 18:17:57 +0200

    API call integrated

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,13 +1,17 @@
+
 function handleSubmit(event) {
     event.preventDefault()
 
+    let baseUrl = "http://api.openweathermap.org/data/2.5/weather?zip=10001";
+    let apiKey = "&appid=feb5077347ee53d4e8314a88815be83d&units=imperial";
+
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('name').value;
 
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch("http://api.openweathermap.org/data/2.5/weather?zip=10001&appid=feb5077347ee53d4e8314a88815be83d&units=imperial")
     .then(res => {
         return res.json()
     })
